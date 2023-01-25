30db3051aa79fc72bd46d282372ddf91170cfea6
FIXER3600 <guilhermecomercial51@gmail.com>
2023-01-25 15:46:01 -0300

    Busca CEP

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import '../models/endereco_model.dart';
+import '../repositories/cep_repository_impl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool loading = false;
+  final CepRepositoryImpl cepRepository = CepRepositoryImpl();
+  EnderecoModel? enderecoModel;
+  final formKey = GlobalKey<FormState>();
+  final cepEC = TextEditingController();
+  @override
+  void dispose() {
+    cepEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buscar CEP'),
+      ),
+      body: SingleChildScrollView(
+          child: Form(
+              key: formKey,
+              child: Column(children: [
+                TextFormField(
+                  controller: cepEC,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'CEP Obrigatório';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                    child: const Text('Buscar'),
+                    onPressed: () async {
+                      final valid = formKey.currentState?.validate() ?? false;
+                      if (valid) {
+                        try {
+                          setState(() {
+                            loading = true;
+                          });
+                          final endereco =
+                              await cepRepository.getCep(cepEC.text);
+                          setState(() {
+                            loading = false;
+                            enderecoModel = endereco;
+                          });
+                        } on Exception {
+                          setState(() {
+                            loading = false;
+                            enderecoModel = null;
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Erro ao Buscar o CEP')));
+                        }
+                      }
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                Visibility(
+                    visible: loading, child: const CircularProgressIndicator()),
+                Visibility(
+                    visible: enderecoModel != null,
+                    child: Text(
+                        "${enderecoModel?.logradouro}, ${enderecoModel?.complemento}, ${enderecoModel?.cep}"))
+              ]))),
+    );
+  }
+}
